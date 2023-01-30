04bb4c825ebf5aef065218061375e687ad29cf49
warencondor1 <73301658+WarenWega@users.noreply.github.com>
2023-01-31 00:24:44 +0100

    Add files via upload

---
+++ firstversion_API.py
@@ -0,0 +1,81 @@
+from flask import Flask, jsonify, request
+import json,csv
+
+
+app = Flask(__name__)
+
+list_of_transactions = []
+
+with open('fichier.csv') as file:
+	reader = csv.reader(file)
+	header = next(reader)
+	for i in reader:
+		sender,receiver,Amount,date=i
+		list_of_transactions.append({"sender":sender,"receiver":receiver,"Amount":float(Amount),"date":date})
+
+
+@app.route('/hello')
+def aff():
+    return "Hello"
+
+@app.route('/display' , methods = ['GET'])
+def index():
+    list_of_transactions.sort(key=lambda x: x['date'])
+    return jsonify(list_of_transactions)
+
+#Afficher les transactions triées d'une personne quelconque
+#curl http://localhost:5000/display_for_someone?p=Salma
+#Le resultat apparaitra dans la console
+@app.route('/display_for_someone' , methods = ['GET'])
+def a():
+    p = request.args.get("p")
+    if p:
+      p_transactions = [i for i in list_of_transactions if i['receiver']==p]
+      p_sorted = sorted(p_transactions,key=lambda x: x['date'])
+      return str(p_sorted)	 
+    return " "
+
+#Afficher le montant d'une personne
+#curl http://localhost:5000/display_balance_of_someone?p=Salma
+@app.route('/display_balance_of_someone' , methods = ['GET'])
+def b():
+    p = request.args.get("p")
+    if p:
+      p_transactions = [i for i in list_of_transactions if i['sender']==p]
+      balance1 = sum([i['Amount'] for i in p_transactions])
+      
+      p_transactions = [i for i in list_of_transactions if i['receiver']==p]
+      balance2 = sum([i['Amount'] for i in p_transactions])
+	
+      if balance1>balance2 :
+       newbalance = balance2 - balance1
+       return "Prennez garde vous avez dépassé le plafond. Pensez à rechager votre compte. Compte:" + str(newbalance)
+
+      if balance2>balance1 :
+       newbalance = balance2 - balance1
+       return "Compte:"+str(newbalance)
+      return " "	 
+
+#Enregister une transaction
+#curl --X POST -H "Content-type:application/json" --data-binary "{\"sender\":\"Pepa\",\"receiver\":\"Salma\",\"Amount\":\100.0\,\"date\":\"2023-01-11\"}" http://localhost:5000/record
+#curl --X POST -H "Content-type:application/json" --data-binary "{\"sender\":\"Waren\",\"receiver\":\"Salma\",\"Amount\":\"200.0\",\"date\":\"2023-01-12\"}" http://localhost:5000/record
+#curl --X POST -H "Content-type:application/json" --data-binary "{\"sender\":\"Ryane\",\"receiver\":\"Tom\",\"Amount\":\"150.0\",\"date\":\"2023-01-13\"}" http://localhost:5000/record
+#curl --X POST -H "Content-type:application/json" --data-binary "{\"sender\":\"Salma\",\"receiver\":\"Albert\",\"Amount\":\"70.0\",\"date\":\"2023-01-14\"}" http://localhost:5000/record
+
+
+@app.route('/record' , methods = ['POST'])
+def add():
+    data = request.get_json()
+    format = {
+		'date' : data['date'], 
+		'sender':data['sender'],
+		'receiver':data['receiver'],
+		'Amount':data['Amount']
+	}
+    list_of_transactions.append(format)
+    return "Magnifique"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
