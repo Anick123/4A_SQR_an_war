c88a55772c29ae122980ef12982b94f0437110c4
warencondor1 <73301658+WarenWega@users.noreply.github.com>
2023-02-01 16:05:34 +0100

    Update firstversion_API.py

--- firstversion_API.py
+++ firstversion_API.py
@@ -85,7 +85,7 @@ with open('fichier.csv') as file:
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == "check_syntax":
-	    print("Build [ OK ]")
+            print("Build [ OK ]")
 	    exit(0)
         else:
             print("Passed argument not supported ! Supported argument: check_syntax")
