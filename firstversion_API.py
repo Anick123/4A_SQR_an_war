7238fc12917d0e8f1f7581855e11006d0133cca2
warencondor1 <73301658+WarenWega@users.noreply.github.com>
2023-02-01 16:06:38 +0100

    Update firstversion_API.py

--- firstversion_API.py
+++ firstversion_API.py
@@ -86,7 +86,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == "check_syntax":
             print("Build [ OK ]")
-	    exit(0)
+            exit(0)
         else:
             print("Passed argument not supported ! Supported argument: check_syntax")
             exit(1)
